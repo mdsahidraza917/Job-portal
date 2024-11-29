8fd8fd42b776c3db5f7f8e12d25467d375eaed73
Md Sahid Raza <89832304+mdsahidraza917@users.noreply.github.com>
2024-11-29 16:16:45 +0530

    1st Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "first.html"));
+});
+
+
+mongoose.connect(process.env.MONGOOSE_URL).then(()=>{
+    console.log("mongoDB is Connected");
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port",PORT);
+})
